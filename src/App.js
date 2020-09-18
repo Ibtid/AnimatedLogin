141f494b7201c4d91932eea99162c2d5a9f446b2
Ibtid <ibtid011@example.com>
2020-09-18 20:18:30 +0600

    removed components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import Login from "./Login/login";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
