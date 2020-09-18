d17a9ffcf4cea5c98e3240e015d605e6ff7d39e4
Ibtid <ibtid011@example.com>
2020-09-18 23:48:28 +0600

    bring register in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import "./App.scss";
 import Login from "./components/login/login";
+import Register from "./components/login/register";
 
 function App() {
-  const [isLogginActive, setisLogginActive] = useState(true);
+  const [isLogginActive, setisLogginActive] = useState(false);
   return (
     <div className="App">
       <div className="login">
-        <div className="container">{isLogginActive && <Login />}</div>
+        <div className="container">
+          {isLogginActive && <Login />}
+          {!isLogginActive && <Register />}
+        </div>
       </div>
     </div>
   );
